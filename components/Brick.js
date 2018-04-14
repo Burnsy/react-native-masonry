3e80c4a8f35531181f8dd9c42bfdde30792b648c
Chris Burns <burnsy@outlook.com>
2018-04-14 11:27:06 +0100

    Added main block

--- components/Brick.js
+++ components/Brick.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 import { View, Image, TouchableHighlight } from 'react-native';
 import Injector from 'react-native-injectable-component';
 
-export default function Brick (props) {
+export default function Brick(props) {
 	// Avoid margins for first element
 	const image = (props.onPress) ? _getTouchableUnit(props, props.gutter) : _getImageTag(props, props.gutter);
 	const footer = (props.renderFooter) ? props.renderFooter(props.data) : null;
 	const header = (props.renderHeader) ? props.renderHeader(props.data) : null;
+	const main = (props.renderHeader) ? props.renderHeader(props.data) : null;
 
 	return (
 		<View key={props.brickKey}>
-		  {header}
-		  {image}
-		  {footer}
+			{header}
+			{main}
+			{footer}
 		</View>
 	);
 }
 
 // _getImageTag :: Image, Gutter -> ImageTag
-export function _getImageTag (props, gutter = 0) {
+export function _getImageTag(props, gutter = 0) {
 	const imageProps = {
 		key: props.uri,
 		source: {
@@ -35,22 +36,22 @@ export function _getImageTag (props, gutter = 0) {
 
 	return (
 		<Injector
-		  defaultComponent={Image}
-		  defaultProps={imageProps}
-		  injectant={props.customImageComponent}
-		  injectantProps={props.customImageProps} />
+			defaultComponent={Image}
+			defaultProps={imageProps}
+			injectant={props.customImageComponent}
+			injectantProps={props.customImageProps} />
 	)
 }
 
 // _getTouchableUnit :: Image, Number -> TouchableTag
-export function _getTouchableUnit (image, gutter = 0) {
+export function _getTouchableUnit(image, gutter = 0) {
 	return (
 		<TouchableHighlight
-          key={image.uri}
-          onPress={() => image.onPress(image.data)}>
-          <View>
-            { _getImageTag(image, gutter) }
-          </View>
+			key={image.uri}
+			onPress={() => image.onPress(image.data)}>
+			<View>
+				{_getImageTag(image, gutter)}
+			</View>
 		</TouchableHighlight>
 	);
 }
